46eee4560357cd31056e97b0ef6adbdd4dbece6c
Ehte22 <syedehteshamali22@gmail.com>
2025-02-13 16:30:51 +0530

    code tested

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -4,7 +4,10 @@ const Loader = ({ size = 6 }) => {
 
     return <>
         <div className="flex justify-center items-center">
-            <div className={`animate-spin rounded-full h-${size} w-${size} border-t-2 border-b-2 border-indigo-600`}></div>
+            <div
+                className="animate-spin rounded-full border-t-2 border-b-2 border-indigo-600"
+                style={{ height: `${size * 4}px`, width: `${size * 4}px` }}
+            ></div>
         </div>
     </>
 }
